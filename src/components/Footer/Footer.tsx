65c3f7ebd5dbe55d97040474d921791076a6d045
ChrisBRN <chriskbrown@hotmail.co.uk>
2025-05-27 09:41:26 +0100

    update: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
 	return (
 		<footer className={styles.container}>
 			<p>
-				A Real Cushon Page is a very Cushon page and completely
+				A Real Cushon Page is a very real Cushon page and completely
 				regulated by the FSCS and similar institutions
 			</p>
 			<p>Number 1, Real street, Real town, Real country, RE1 1LY</p>
